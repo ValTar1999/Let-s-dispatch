f0f6fa3c473b07dd16211693d592792c287ea694
fauyds <www.vladtarna25@gmail.com>
2023-03-02 01:00:39 +0200

    -+-+-

--- main.js
+++ main.js
@@ -69,4 +69,52 @@ closePopUp.addEventListener('click', () => {
 
 
 
+ const form = document.forms["form"];
+ const button = form.elements["button"];
+
+ const inputArr = Array.from(form);
+ const validInputArr = [];
  
+ inputArr.forEach((el) => {
+    if (el.hasAttribute("data-reg")) {
+        el.setAttribute("is-valide", "0");
+        validInputArr.push(el);
+    }
+ });
+
+ 
+
+ form.addEventListener("input", inputHandler);
+ button.addEventListener("click", buttonHandler);
+
+ function inputHandler({target}) {
+    if (target.hasAttribute("data-reg")) {
+        inputCheck(target);
+    }
+ }
+
+ function inputCheck(el) {
+    const inputValue = el.value;
+    const inputReg = el.getAttribute("data-reg");
+    const reg = new RegExp(inputReg);
+    if (reg.test(inputValue)) {
+        el.style.border = "2px solid rgb(0 194 255)";
+        el.setAttribute("is-valide", "1");
+    } else {
+        el.style.border = "2px solid rgb(255 0 0)";
+        el.setAttribute("is-valide", "0");
+    }
+ }
+
+ function buttonHandler(e) {
+    const isAllValid = [];
+    validInputArr.forEach((el) => {
+        isAllValid.push(el.getAttribute("is-valid"));
+    });
+    const isValid = isAllValid.reduce((acc, current) => {
+        return acc && current;
+    });
+    if(!Boolean(Number(isValid))) {
+        e.preventDefault();
+    }
+ }
